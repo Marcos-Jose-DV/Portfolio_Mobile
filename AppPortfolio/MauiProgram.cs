d74f615e149ce34bd7277018b42471f5cf9b6adf
Marcos José <marcosjoseDV@hotmail.com>
2024-02-06 19:42:05 -0300

    Criando Pages:
    adicionando estilo, fonte e icone.
    page: home, concluido.
    page: tabar criando e configurando com icones e o titulo
    notas: ainda falta algo para colocar na page home, abaixo da image.

--- AppPortfolio/MauiProgram.cs
+++ AppPortfolio/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace AppPortfolio
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("NunitoSans-10pt-Bold.ttf", "NunitoSans-Bold");
                 });
 
 #if DEBUG
